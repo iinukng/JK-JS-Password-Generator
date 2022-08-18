44a0d524f00d7684676c99957fd39bb8ef941754
justin <iinukng@gmail.com>
2022-08-18 17:51:29 -0400

    fixed the prompts so the randomizer generates passwords properly.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # JK-JS-Password-Generator
+
+## Description
+
+This password generator test my 
+
+![](assets/images/03-javascript-homework-demo.png)
--- script.js
+++ script.js
@@ -1,45 +1,46 @@
 // Arrays 
+var length = 8;
 var outCome = [];
-var passwordLength = 8;
 
-var numList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var symList = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
-var lowList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var uppList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numList = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var symList = ['!', '@', '#', '$', '%', '^', '&', '*'];
+var lowList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var uppList = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
-  var correctPrompts = getPrompt();
+  var correctPrompts = Prompt();
   var passwordText = document.querySelector("#password");
 
   if(correctPrompts) {
-    var newPassword = generatePassword();
-    passwordText.value = newPassword;
-  }
+    var password = generatePassword();
+    passwordText.value = password;
+  } 
 }
 
 function generatePassword() {
   var password = "";
-  for(var i = 0; i < passwordLength; i++) {
-    var randomLetter = Math.floor(Math.random() * outCome.length);
-    password = password + outCome[randomLetter];
+  for(var i = 0; i < length; i++) {
+      var random = Math.floor(Math.random() * outCome.length);
+      password = password + outCome[random];
   }
   return password;
 }
 
-function getPrompt() {
-  var outCome = [];
-  var passwordLength = parseInt(window.prompt("How long do you want your password to be? Pick from 8 to 128 characters."));
+function Prompt() {
 
-  if(isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+  length = parseInt(window.prompt("How long do you want your password to be? Pick from 8 to 128 characters."));
+
+  if(isNaN(length) || length < 8 || length > 128) {
     window.alert("You should have 8 to 128 characters in your password!");
-    return;
+    return false;
   }
   if (window.confirm("Do you want numbers in your list?")) {
     outCome = outCome.concat(numList);
@@ -53,5 +54,6 @@ function getPrompt() {
   if (window.confirm("Do you want uppercase letters in your list?")) {
     outCome = outCome.concat(uppList);
   }
-  return;
+  return true;
 }
+
