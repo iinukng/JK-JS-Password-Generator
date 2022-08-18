6532fbbe9000fc7be546e6df65f0a3619670c501
justin <iinukng@gmail.com>
2022-08-17 22:04:25 -0400

    added arrays for possible password outcomes.

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+// Arrays 
+var outCome = [];
+var passwordLength = 128;
+
+var numList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var symList = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+var LowList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var UppList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
@@ -9,6 +21,3 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
